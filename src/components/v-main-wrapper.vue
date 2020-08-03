7dc6e822355fef17dd2bab38b41aa387218c271f
Daria Menscikova <menscikova.daria@gmail.com>
2020-08-03 16:52:05 +0200

    added styles and main components

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="v-main-wrapper">
+    <vCatalog/>
+    <vCart/>
+  </div>
+</template>
+
+<script>
+import vCatalog from './v-catalog';
+import vCart from './v-cart';
+
+export default {
+  name: 'v-main-wrapper',
+  components: {
+    vCatalog,
+    vCart
+  }
+}
+</script>
+
+<style>
+.v-main-wrapper {
+  display: flex;
+  margin: 0 auto;
+}
+</style>

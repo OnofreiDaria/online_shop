736ddf8099088d93f900b8324092c3628657344c
Daria Menscikova <menscikova.daria@gmail.com>
2020-08-06 12:14:56 +0200

    added sorting by category

--- src/components/catalog/v-catalog.vue
+++ src/components/catalog/v-catalog.vue
@@ -5,8 +5,13 @@
     </router-link>
 
     <h1>Catalog</h1>
+    <vSelect :selected="selected"
+             :options="categories"
+             @select="sortByCategories"
+    />
+
     <div class="v-catalog__list">
-      <vCatalogItem v-for="product in PRODUCTS"
+      <vCatalogItem v-for="product in filteredProducts"
                     :key="product.article"
                     :product_data="product"
                     @addToCart="addToCart"
@@ -18,22 +23,48 @@
 <script>
 import vCatalogItem from './v-catalog-item'
 import { mapActions, mapGetters } from 'vuex'
+import vSelect from '../v-select'
 
 export default {
   name: "vCatalog",
   data() {
-    return  {}
+    return  {
+      categories: [
+        { name: 'Все', value: 'all' },
+        { name: 'Мужские', value: 'm' },
+        { name: 'Женские', value: 'f' }
+      ],
+      selected: 'All',
+      sortedProducts: []
+    }
   },
   computed: {
     ...mapGetters([
         'PRODUCTS',
-      'CART'])
+      'CART']),
+    filteredProducts() {
+      if(this.sortedProducts.length) {
+        return this.sortedProducts
+      } else {
+        return this.PRODUCTS
+      }
+    }
   },
   methods: {
     ...mapActions ([
       'GET_PRODUCT_FROM_API',
       'ADD_TO_CART'
     ]),
+    sortByCategories(category) {
+      this.sortedProducts = [];
+      let vm = this;
+      this.PRODUCTS.map(item => {
+        if(item.category === category.name) {
+          vm.sortedProducts.push(item);
+        }
+      })
+      this.selected = category.name
+    },
     addToCart(data) {
       this.ADD_TO_CART(data)
     }
@@ -47,7 +78,8 @@ export default {
       })
   },
   components: {
-    vCatalogItem
+    vCatalogItem,
+    vSelect
   }
 }
 </script>
